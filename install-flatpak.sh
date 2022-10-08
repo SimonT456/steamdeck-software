4833d08ee176f588210b19faeaf439a51113bdf4
SimonT456 <12100172+SimonT456@users.noreply.github.com>
2022-10-08 17:58:14 +0200

    Update install-flatpak.sh

--- install-flatpak.sh
+++ install-flatpak.sh
@@ -1,4 +1,4 @@
 # Add any other Flatpak package you want to install, separated by spaces
-PACKAGES="app.ytmdesktop.ytmdesktop com.bitwarden.desktop com.discordapp.Discord com.github.alexkdeveloper.desktop-files-creator com.github.k4zmu2a.spacecadetpinball com.google.Chrome com.mattjakeman.ExtensionManager com.steamgriddb.SGDBoop org.videolan.VLC sh.ppy.osu tv.plex.PlexHTPC"
+PACKAGES="app.ytmdesktop.ytmdesktop com.bitwarden.desktop com.discordapp.Discord com.github.alexkdeveloper.desktop-files-creator com.github.k4zmu2a.spacecadetpinball com.google.Chrome com.mattjakeman.ExtensionManager com.steamgriddb.SGDBoop org.videolan.VLC sh.ppy.osu tv.plex.PlexHTPC net.lutris.Lutris"
 
 flatpak install $PACKAGES
